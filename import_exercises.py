494f061fdf7daa0ed5a1903e54792ed211421cc7
Rabeka Branham <rabeka.june@gmail.com>
2023-10-30 12:00:11 -0500

    updated after review

--- import_exercises.py
+++ import_exercises.py
@@ -1,8 +1,5 @@
 # Create a file named import_exercises.py. Within this file, use from to import the calculate_tip 
 # function directly. Call this function with values you choose and print the result.
 
-# from function_exercises import is_two
-# print(is_two(7))
-
-import itertools
-itertools.count(10)
+from function_exercises import calculate_tip
+print(calculate_tip(100,.5))
